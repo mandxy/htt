9806bbf817c843f9bbaaca5173ecd910a372e5a6
hahahrfool <75717694+hahahrfool@users.noreply.github.com>
2020-12-13 09:40:53 +0000

    Update htt_ac.js

--- Scripts/HTT/htt_ac.js
+++ Scripts/HTT/htt_ac.js
@@ -1,33 +1,39 @@
 //2020.12.05 移除报错模块 时段奖励、视频签到奖励，阅读时长奖励请自行点击领取
 
-//2020.12.13, hahahrfool: edit
+//2020.12.13, hahahrfool: bug fix, adapt for ac
 
 const Notice = 1; //设置运行多少次才通知。
 const log = 1; //设置0关闭日志,1开启日志
 
+const $ = new Env("htt");
+
 //====================================
 
-const $iosrule = iosrule(); //声明必须
 const httid = "A";
 const huitoutiao = "惠头条";
 
 //++++++++++++++++++++++++++++++++-
 
-const htt_videoname = "htt_videoname" + httid;
-const htt_video = $iosrule.read(htt_videoname);
+//const htt_videoname = "htt_videoname" + httid;
 
-const htt_dongfangname = "htt_dongfangname" + httid;
-const htt_dongfang = $iosrule.read(htt_dongfangname);
-const htt_smvideoname = "htt_smvideoname" + httid;
-const htt_smvideo = $iosrule.read(htt_smvideoname);
+const htt_video = process.env.htt_videoname
 
-const htt_signurlckname = "htt_signurlckname" + httid;
-const htt_signurlck = $iosrule.read(htt_signurlckname);
+//const htt_dongfangname = "htt_dongfangname" + httid;
+const htt_dongfang =process.env.htt_dongfangname
 
-const htt_signbdname = "htt_signbdname" + httid;
-const htt_signbd = $iosrule.read(htt_signbdname);
+//const htt_smvideoname = "htt_smvideoname" + httid;
+const htt_smvideo = process.env.htt_smvideoname
+
+//const htt_signurlckname = "htt_signurlckname" + httid;
+const htt_signurlck = process.env.htt_signurlckname
+
+//const htt_signbdname = "htt_signbdname" + httid;
+const htt_signbd = process.env.htt_signbdname
+
 var htt_num = 0;
 var htt_result = "";
+
+notify = $.isNode() ? require("./sendNotify") : "";
 
 //++++++++++++++++++++++++++++++++
 
@@ -52,7 +58,7 @@ function htt_read_dongfang() {
     timeout: 60,
   };
 
-  $iosrule.post(llUrl1, function (error, response, data) {
+  $.post(llUrl1, function (error, response, data) {
     if (log == 1) console.log("阅读" + data);
     var obj = JSON.parse(data);
 
@@ -93,7 +99,7 @@ function htt_read_video() {
     timeout: 60,
   };
 
-  $iosrule.post(llUrl1, function (error, response, data) {
+  $.post(llUrl1, function (error, response, data) {
     if (log == 1) console.log("视频" + data);
     var obj = JSON.parse(data);
 
@@ -133,7 +139,7 @@ function htt_read_smvideo() {   //hahahrfool: since it's the last one called, we
     timeout: 60,
   };
 
-  $iosrule.post(llUrl1, function (error, response, data) {
+  $.post(llUrl1, function (error, response, data) {
 
     if(error != null){
         console.log("htt error: "+error);
@@ -189,7 +195,7 @@ function htt_readtotal() {
     timeout: 60,
   };
 
-  $iosrule.post(llUrl1, function (error, response, data) {
+  $.post(llUrl1, function (error, response, data) {
     var obj = JSON.parse(data);
 
     if (obj.statusCode == 200) {
@@ -224,24 +230,9 @@ function htt_msg(r) {
   htt_result += r;
   if (log == 1) console.log(htt_num);
   if (htt_num == 7) {
-    var loon = $iosrule.read("iosrule");
-    if (typeof loon != "undefined") {
-      loon = loon.substring(7, loon.length);
-      loon++;
-      console.log("惠头条第" + loon + "次运行");
-      $iosrule.write("iosrule" + loon, "iosrule");
-    } else {
-      loon = 1;
-      $iosrule.write("iosrule" + loon, "iosrule");
-    }
-    if (loon % Notice == 0) {
-      papa(tt, "[签到-时段-视频-阅读]" + "当前运行" + loon + "次", htt_result);
-      loon = 0;
-      $iosrule.write("iosrule" + loon, "iosrule");
-      loon = 0;
-      htt_result = "";
-      $iosrule.write("iosrule" + loon, "iosrule");
-    }
+    
+      sendNotifyFuncWrapper(tt, "[签到-时段-视频-阅读]", htt_result);
+    
   }
 }
 
@@ -290,8 +281,8 @@ function formatSeconds(value) {
   return res;
 }
 
-function papa(x, y, z) {
-  $iosrule.notify(x, y, z);
+function sendNotifyFuncWrapper(x, y, z) {
+    notify.sendNotify(x + "\n" + y + "\n" + z);
 }
 
 function sign(code) {
@@ -303,53 +294,7 @@ function sign(code) {
   return c;
 }
 
-function iosrule() {
-  const isRequest = typeof $request != "undefined";
-  const isSurge = typeof $httpClient != "undefined";
-  const isQuanX = typeof $task != "undefined";
-  const notify = (title, subtitle, message) => {
-    if (isQuanX) $notify(title, subtitle, message);
-    if (isSurge) $notification.post(title, subtitle, message);
-  };
-  const write = (value, key) => {
-    if (isQuanX) return $prefs.setValueForKey(value, key);
-    if (isSurge) return $persistentStore.write(value, key);
-  };
-  const read = (key) => {
-    if (isQuanX) return $prefs.valueForKey(key);
-    if (isSurge) return $persistentStore.read(key);
-  };
-  const get = (options, callback) => {
-    if (isQuanX) {
-      if (typeof options == "string") options = { url: options };
-      options["method"] = "GET";
-      $task.fetch(options).then(
-        (response) => {
-          response["status"] = response.statusCode;
-          callback(null, response, response.body);
-        },
-        (reason) => callback(reason.error, null, null)
-      );
-    }
-    if (isSurge) $httpClient.get(options, callback);
-  };
-  const post = (options, callback) => {
-    if (isQuanX) {
-      if (typeof options == "string") options = { url: options };
-      options["method"] = "POST";
-      $task.fetch(options).then(
-        (response) => {
-          response["status"] = response.statusCode;
-          callback(null, response, response.body);
-        },
-        (reason) => callback(reason.error, null, null)
-      );
-    }
-    if (isSurge) $httpClient.post(options, callback);
-  };
-  const end = () => {
-    if (isQuanX) isRequest ? $done({}) : "";
-    if (isSurge) isRequest ? $done({}) : $done();
-  };
-  return { isRequest, isQuanX, isSurge, notify, write, read, get, post, end };
-}
+
+// prettier-ignore
+function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,o)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let o=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");o=o?1*o:20,o=e&&e.timeout?e.timeout:o;const[r,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:o},headers:{"X-Key":r,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),o=JSON.stringify(this.data);s?this.fs.writeFileSync(t,o):i?this.fs.writeFileSync(e,o):this.fs.writeFileSync(t,o)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let o=t;for(const t of i)if(o=Object(o)[t],void 0===o)return s;return o}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),o=s?this.getval(s):"";if(o)try{const t=JSON.parse(o);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,o]=/^@(.*?)\.(.*?)$/.exec(e),r=this.getval(i),h=i?"null"===r?null:r||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,o,t),s=this.setval(JSON.stringify(e),i)}catch(e){const r={};this.lodash_set(r,o,t),s=this.setval(JSON.stringify(r),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)}):this.isQuanX()?$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:o,body:r}=t;e(null,{status:s,statusCode:i,headers:o,body:r},r)},t=>e(t)):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:o,body:r}=t;e(null,{status:s,statusCode:i,headers:o,body:r},r)},t=>e(t)))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:o,body:r}=t;e(null,{status:s,statusCode:i,headers:o,body:r},r)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:o,body:r}=t;e(null,{status:s,statusCode:i,headers:o,body:r},r)},t=>e(t))}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",o){const r=t=>{if(!t||!this.isLoon()&&this.isSurge())return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,r(o)):this.isQuanX()&&$notify(e,s,i,r(o)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
+
